495164268ded140af94f388e90f4140aded3bd18
brandonisthebomb <liu.b97@gmail.com>
2014-03-29 14:41:08 -0400

    test

--- Pilot/app/src/main/java/com/Pilot/pilot/app/CoolButton.java
+++ Pilot/app/src/main/java/com/Pilot/pilot/app/CoolButton.java
@@ -5,11 +5,6 @@ import android.widget.Button;
 /**
  * Created by Brandon on 3/29/14.
  */
-<<<<<<< HEAD
-public class CoolButton {
-    test
-=======
 public class CoolButton extends Button {
     return
->>>>>>> FETCH_HEAD
 }
