a29e4387bf705c550bd86126a89ad66509ef9f85
weiya711 <oliviahsu1107@gmail.com>
2014-03-29 16:38:56 -0400

    hello

--- Pilot/app/src/main/java/com/Pilot/pilot/app/MainActivity.java
+++ Pilot/app/src/main/java/com/Pilot/pilot/app/MainActivity.java
@@ -14,6 +14,7 @@ public class MainActivity extends ActionBarActivity {
     {
         Intent intent = new Intent(this, createGoalActivity.class);
         startActivity(intent);
+        hello
     }
 
     public void seeProgressScreen(View view)
