cda01100ea5449f491a7c41d1767a04a910e3d6e
brandonisthebomb <liu.b97@gmail.com>
2014-03-29 17:17:02 -0400

    major changessss

---
+++ Pilot/app/src/main/java/com/Pilot/pilot/app/myReceiver.java
@@ -0,0 +1,23 @@
+package com.Pilot.pilot.app;
+
+/**
+ * Created by Brandon on 3/29/14.
+ */
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+public class myReceiver extends BroadcastReceiver {
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        // assumes WordService is a registered service
+        intent = new Intent(context, MainActivity.class);
+        if (intent.getAction()!=null) {
+            intent.getAction().equals(Intent.ACTION_USER_PRESENT)
+            context.startService(intent);
+        }
+    }
+}
+
