e771b2a8b4a0b6c3f93e3c7ed8ed6cab6374ba86
weiya711 <oliviahsu1107@gmail.com>
2014-03-30 01:28:24 -0400

    motivation button done

--- Pilot/app/src/main/java/com/Pilot/pilot/app/quoteActivity.java
+++ Pilot/app/src/main/java/com/Pilot/pilot/app/quoteActivity.java
@@ -1,17 +1,22 @@
 package com.Pilot.pilot.app;
 
 import android.app.Activity;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.Gravity;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Random;
+import java.util.logging.Handler;
 
 
 public class quoteActivity extends Activity {
+    int[] imageArray = {R.drawable.antelope_quote1, R.drawable.pond_quote2, R.drawable.brushes_quote3, R.drawable.desert_quote4,
+    R.drawable.ducks_quote5, R.drawable.moon_quote6};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,9 +28,13 @@ public class quoteActivity extends Activity {
         {
             BufferedReader reader = new BufferedReader(new InputStreamReader(getAssets().open("quotes.txt")));
 
+
+
             int numlines = new Integer(9);
+            int numimages = new Integer(6);
             Random r = new Random();
             int desiredLine = r.nextInt(numlines);
+            int desiredImage = r.nextInt(numimages);
 
             int lineCtr = 0;
             while ((text = reader.readLine()) != null)   {
@@ -38,15 +47,20 @@ public class quoteActivity extends Activity {
 
 
             reader.close();
+            ImageView backDisplay = (ImageView)findViewById(R.id.imageView1);
+            backDisplay.setBackgroundResource(imageArray[desiredImage]);
+
 
             TextView quoteDisplay = (TextView)findViewById(R.id.quoteDisplay);
             quoteDisplay.setText('"'+text+'"');
             quoteDisplay.setGravity(Gravity.CENTER);
+            quoteDisplay.setTextColor(Color.WHITE);
             quoteDisplay.setTextSize(35);
 
             TextView authorDisplay = (TextView)findViewById(R.id.authorDisplay);
             authorDisplay.setText("-" + author);
             authorDisplay.setGravity(Gravity.RIGHT);
+            authorDisplay.setTextColor(Color.WHITE);
         }
         catch (IOException e)
         {
