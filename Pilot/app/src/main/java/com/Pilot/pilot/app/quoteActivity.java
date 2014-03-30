ba6ef5d79a859d0b5639b57bbef818f46e9d3382
brandonisthebomb <liu.b97@gmail.com>
2014-03-30 01:35:10 -0400

    n

--- Pilot/app/src/main/java/com/Pilot/pilot/app/quoteActivity.java
+++ Pilot/app/src/main/java/com/Pilot/pilot/app/quoteActivity.java
@@ -11,7 +11,6 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Random;
-import java.util.logging.Handler;
 
 
 public class quoteActivity extends Activity {
